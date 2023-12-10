2d14d234dc9490061f948ce04e4e6c79856e90c3
--global <rhyanjacksonmarinho@gmail.com>
2023-12-10 00:20:40 +0000

    Function that receives json and catchJsonInfos and getCategories done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from apikey import api_key
 import os
 import json
+from math import radians, cos, sin, acos
+
 
 def menu():
     print(
@@ -34,6 +36,7 @@ def menu():
             if not 1 <= op or not op <= 2:
                 print("Insert an valid option.")
             else:
+                clear_terminal()
                 return op
 
 
@@ -66,19 +69,43 @@ def getCategories(categories_database):
         category_string = ""
         category_list = []
         while True:
-            print()
-            print("Choose the category that you want, this maybe have subcategories.\n")
-            print(f"Category directory > {category_string}\n")
-            for category in current.keys():
-                quantity_sub = len(current[category].keys())
-                print(f"{category}({quantity_sub})")
-            print()
+            while True:
+                clear_terminal()
+                print()
+                print(
+                    "Choose the category that you want | example: category(n° of sub.)\n"
+                )
+                print(f"Category directory > {category_string}\n")
+                print("( ID ) | Category")
+                print("―――――――――――――――――――")
+                for pos, category in enumerate(current.keys()):
+                    quantity_sub = len(current[category].keys())
+                    print(f"( {str(pos).zfill(2)} ) | {category}({quantity_sub})")
+                print()
+                try:
+                    next_category_input = input(
+                        "Insert the next category ID that you want (Enter to actual) > "
+                    )
+                    next_category_id = int(next_category_input)
+                except Exception:
+                    if next_category_input.strip() == "":
+                        break
+                    print("Insert an valid ID. (Error)")
+                else:
+                    if not 0 <= next_category_id <= len(current.keys()) - 1:
+                        print("Insert an valid ID.")
+                    else:
+                        break
 
-            next_category = input("Insert the next category that you want > ")
+            if next_category_input.strip() == "":
+                next_category = ""
+            else:
+                next_category = list(current.keys())[next_category_id]
 
-            clear_terminal()
+            if next_category == "" and "".join(category_list) == "":
+                break
 
-            if next_category.strip() == "":
+            elif next_category == "":
                 categories_selected.append(category_string)
                 break
 
@@ -90,15 +117,18 @@ def getCategories(categories_database):
             if len(current.keys()) == 0:
                 categories_selected.append(category_string)
                 break
-            pass_var = True
+
+            clear_terminal()
+        pass_var = True
+        print(f'Already selected > {", ".join(categories_selected)}')
         print(
-                """
+            """
 ---------< Do you want to continue selecting? >---------
  > [1] Yes
  > [Any other option] No
                 
                 """
-            )
+        )
         while True:
             op = input("\nInsert your option > ")
             if op == "1":
@@ -112,7 +142,62 @@ def getCategories(categories_database):
     return categories_selected
 
 
-# Treating the category data
+def distanceByCoords(lat1, lat2, lng1, lng2):
+    lat1Rad, lat2Rad, lng1Rad, lng2Rad = radians(lat1), radians(float(lat2)), radians(lng1), radians(float(lng2))
+    rTerra = 6371
+
+    return (
+        acos(
+            sin(lat1Rad) * sin(lat2Rad)
+            + cos(lat1Rad) * cos(lat2Rad) * cos(lng2Rad - lng1Rad)
+        )
+        * rTerra
+    )
+
+
+def catchJsonInfos(dict_from_json, lat_client, lon_client):
+    data = dict_from_json
+
+    places_organized_list = []
+
+    for features in data["features"]:
+        try:
+            name = features["properties"]["name"]
+            country = features["properties"]["country"]
+            city = features["properties"]["city"]
+            lat = features["properties"]["lat"]
+            lon = features["properties"]["lon"]
+            distances = distanceByCoords(lat, lat_client, lon, lon_client)
+            district = features["properties"]["district"]
+            adress = features["properties"]["formatted"]
+            categories = features["properties"]["categories"]
+        except KeyError:
+            continue
+        else:
+            info_dict = {
+                "name": name,
+                "country": country,
+                "city": city,
+                "lat_long": (lat, lon),
+                "distance": distances,
+                "district": (district),
+                "formatted": adress,
+                "categories": categories,
+            }
+
+            places_organized_list.append(info_dict)
+
+    for item in places_organized_list:
+        print("Name:", item["name"])
+        print("Country:", item["country"])
+        print("City:", item["city"])
+        print("Latitude and Longitude:", item["lat_long"])
+        print("District:", item["district"])
+        print("Distance:", "{:.2f}".format(item["distance"]), "Km")
+        print("Adress:", item["formatted"])
+        print("Categories:", item["categories"])
+        print("----")
+
 
 def main():
     database = fileToDict("categories2.txt")
@@ -125,18 +210,21 @@ def main():
         exit()
 
     # Requesting the information input's.
-    local_coords_lat = input("Insert your latitude > ")
-    local_coords_lon = input("Insert your longitude > ")
-    how_far_meters = float(input("Insert how far you want to go (km) > ")) * 1000
+    # local_coords_lat = input("Insert your latitude > ")
+    # local_coords_lon = input("Insert your longitude > ")
+    # how_far_meters = float(input("Insert how far you want to go (km) > ")) * 1000
+
+    local_coords_lat = "40.730610"
+    local_coords_lon = "-73.935242"
+    how_far_meters = 20 * 1000
 
     # Treating data to make the request in API
-
-    area = f"circle:{local_coords_lon},{local_coords_lat},{how_far_meters}"
-    url = f"https://api.geoapify.com/v2/places?categories={','.join(selected)}&filter={area}&apiKey={api_key}"
+    url = f"https://api.geoapify.com/v2/places?categories={','.join(selected)}&filter=circle:{local_coords_lon},{local_coords_lat},{how_far_meters}&bias=proximity:{local_coords_lon},{local_coords_lat}&limit=20&apiKey={api_key}"
 
     response = requests.get(url)
-    print(response.json())
+    catchJsonInfos(response.json(), local_coords_lat, local_coords_lon)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     clear_terminal()
     main()
